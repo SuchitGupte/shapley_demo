b42af92903f149880cd5c8ddd1661526bb0083bb
Suchit Gupte <116911107+SuchitGupte@users.noreply.github.com>
2024-04-01 07:05:49 -0400

    Update app.py

--- app.py
+++ app.py
@@ -624,7 +624,8 @@ with tab_framework:
         col1, col2, col3 = st.columns(3)
 
         with col2:
-            st.markdown("$f(x^e) = \Phi_0 + \sum_{i=1}^{|D|} \Phi_i$")
+            # st.markdown("$f(x^e) = \Phi_0 + \sum_{i=1}^{|D|} \Phi_i$")
+            st.image('experror.jpg', caption='Additive property of Shapley values: Motivation for Explanation Error')
 
         st.markdown(
         """
